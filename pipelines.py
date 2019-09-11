764ad90f994b56c4101131cbe76241f584cc6424
Charlie <N1046401@ntub.edu.tw>
2019-09-11 21:07:34 +0800

    改成csv格式存檔

--- pipelines.py
+++ pipelines.py
@@ -4,21 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import csv
 
 class DemoPipeline(object):
     count = 0
     def process_item(self, item, spider):
         DemoPipeline.count += 1
         try:
-        	if DemoPipeline.count == 1:
-        		fobj = open("price.txt","wt")
-        	else:
-        		fobj = open("price.txt","at")
-        	for i,j in zip(item['title'],item['price']):
-        		fobj.write(str(i)+","+str(j)+'\n')
-        		print("item:",str(i),",","price:",str(j))
-        	fobj.close()
+            fobj = open('output.csv','a+')
+            writer = csv.writer(fobj)
+            if DemoPipeline.count == 1:
+                writer.writerow(['title','price'])
+            else:
+                for i,j in zip(item['title'],item['price']):
+                    writer.writerow([str(i),str(j)])
+                    print('item:',str(i),' , ','price',str(j))
         except Exception as err:
         	print(err)
         return item
+
